7c64ff3620b55a2eb8235ef19a04cec932da8c0a
JuanBSoares <juanbsoares@outlook.com>
2024-12-31 20:38:37 -0300

    including new commands tests - show and state

---
+++ 9-show-state-import-refresh/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "juanborges-terraform-commands-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "juanborges-terraform-commands-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "juanborges-terraform-commands-3"
+}
